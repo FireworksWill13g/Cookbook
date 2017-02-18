cebda2b525a81c39eb05e20a6a0006654a1310ad
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-02-18 17:52:14 -0600

    Final push
    done with base tutorial, may change it later

--- Cookbook/formMain.Designer.cs
+++ Cookbook/formMain.Designer.cs
@@ -34,12 +34,16 @@
             this.labelIngredients = new System.Windows.Forms.Label();
             this.buttonAddRecipe = new System.Windows.Forms.Button();
             this.textBoxRecipeName = new System.Windows.Forms.TextBox();
+            this.buttonUpdateName = new System.Windows.Forms.Button();
+            this.listBoxAllIngredients = new System.Windows.Forms.ListBox();
+            this.labelAllIngredients = new System.Windows.Forms.Label();
+            this.buttonAddToRecipe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblRecipes
             // 
             this.lblRecipes.AutoSize = true;
-            this.lblRecipes.Location = new System.Drawing.Point(51, 188);
+            this.lblRecipes.Location = new System.Drawing.Point(39, 132);
             this.lblRecipes.Name = "lblRecipes";
             this.lblRecipes.Size = new System.Drawing.Size(46, 13);
             this.lblRecipes.TabIndex = 0;
@@ -48,24 +52,24 @@
             // listRecipes
             // 
             this.listRecipes.FormattingEnabled = true;
-            this.listRecipes.Location = new System.Drawing.Point(42, 229);
+            this.listRecipes.Location = new System.Drawing.Point(42, 148);
             this.listRecipes.Name = "listRecipes";
-            this.listRecipes.Size = new System.Drawing.Size(357, 264);
+            this.listRecipes.Size = new System.Drawing.Size(153, 82);
             this.listRecipes.TabIndex = 1;
             this.listRecipes.SelectedIndexChanged += new System.EventHandler(this.listRecipes_SelectedIndexChanged);
             // 
             // listIngredients
             // 
             this.listIngredients.FormattingEnabled = true;
-            this.listIngredients.Location = new System.Drawing.Point(427, 229);
+            this.listIngredients.Location = new System.Drawing.Point(42, 258);
             this.listIngredients.Name = "listIngredients";
-            this.listIngredients.Size = new System.Drawing.Size(357, 264);
+            this.listIngredients.Size = new System.Drawing.Size(153, 225);
             this.listIngredients.TabIndex = 3;
             // 
             // labelIngredients
             // 
             this.labelIngredients.AutoSize = true;
-            this.labelIngredients.Location = new System.Drawing.Point(436, 188);
+            this.labelIngredients.Location = new System.Drawing.Point(39, 242);
             this.labelIngredients.Name = "labelIngredients";
             this.labelIngredients.Size = new System.Drawing.Size(59, 13);
             this.labelIngredients.TabIndex = 2;
@@ -73,25 +77,67 @@
             // 
             // buttonAddRecipe
             // 
-            this.buttonAddRecipe.Location = new System.Drawing.Point(42, 86);
+            this.buttonAddRecipe.Location = new System.Drawing.Point(42, 90);
             this.buttonAddRecipe.Name = "buttonAddRecipe";
             this.buttonAddRecipe.Size = new System.Drawing.Size(75, 23);
             this.buttonAddRecipe.TabIndex = 4;
             this.buttonAddRecipe.Text = "Add Recipe";
             this.buttonAddRecipe.UseVisualStyleBackColor = true;
+            this.buttonAddRecipe.Click += new System.EventHandler(this.buttonAddRecipe_Click);
             // 
             // textBoxRecipeName
             // 
-            this.textBoxRecipeName.Location = new System.Drawing.Point(42, 41);
+            this.textBoxRecipeName.Location = new System.Drawing.Point(42, 64);
             this.textBoxRecipeName.Name = "textBoxRecipeName";
-            this.textBoxRecipeName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRecipeName.Size = new System.Drawing.Size(197, 20);
             this.textBoxRecipeName.TabIndex = 5;
+            // 
+            // buttonUpdateName
+            // 
+            this.buttonUpdateName.Location = new System.Drawing.Point(123, 90);
+            this.buttonUpdateName.Name = "buttonUpdateName";
+            this.buttonUpdateName.Size = new System.Drawing.Size(116, 23);
+            this.buttonUpdateName.TabIndex = 6;
+            this.buttonUpdateName.Text = "Update Name";
+            this.buttonUpdateName.UseVisualStyleBackColor = true;
+            this.buttonUpdateName.Click += new System.EventHandler(this.buttonUpdateName_Click);
+            // 
+            // listBoxAllIngredients
+            // 
+            this.listBoxAllIngredients.FormattingEnabled = true;
+            this.listBoxAllIngredients.Location = new System.Drawing.Point(263, 148);
+            this.listBoxAllIngredients.Name = "listBoxAllIngredients";
+            this.listBoxAllIngredients.Size = new System.Drawing.Size(153, 225);
+            this.listBoxAllIngredients.TabIndex = 8;
+            // 
+            // labelAllIngredients
+            // 
+            this.labelAllIngredients.AutoSize = true;
+            this.labelAllIngredients.Location = new System.Drawing.Point(260, 132);
+            this.labelAllIngredients.Name = "labelAllIngredients";
+            this.labelAllIngredients.Size = new System.Drawing.Size(59, 13);
+            this.labelAllIngredients.TabIndex = 7;
+            this.labelAllIngredients.Text = "Ingredients";
+            // 
+            // buttonAddToRecipe
+            // 
+            this.buttonAddToRecipe.Location = new System.Drawing.Point(341, 379);
+            this.buttonAddToRecipe.Name = "buttonAddToRecipe";
+            this.buttonAddToRecipe.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddToRecipe.TabIndex = 9;
+            this.buttonAddToRecipe.Text = "Add To Recipe";
+            this.buttonAddToRecipe.UseVisualStyleBackColor = true;
+            this.buttonAddToRecipe.Click += new System.EventHandler(this.buttonAddToRecipe_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 540);
+            this.Controls.Add(this.buttonAddToRecipe);
+            this.Controls.Add(this.listBoxAllIngredients);
+            this.Controls.Add(this.labelAllIngredients);
+            this.Controls.Add(this.buttonUpdateName);
             this.Controls.Add(this.textBoxRecipeName);
             this.Controls.Add(this.buttonAddRecipe);
             this.Controls.Add(this.listIngredients);
@@ -114,6 +160,10 @@
         private System.Windows.Forms.Label labelIngredients;
         private System.Windows.Forms.Button buttonAddRecipe;
         private System.Windows.Forms.TextBox textBoxRecipeName;
+        private System.Windows.Forms.Button buttonUpdateName;
+        private System.Windows.Forms.ListBox listBoxAllIngredients;
+        private System.Windows.Forms.Label labelAllIngredients;
+        private System.Windows.Forms.Button buttonAddToRecipe;
     }
 }
 
--- Cookbook/formMain.cs
+++ Cookbook/formMain.cs
@@ -26,7 +26,7 @@ namespace Cookbook
         private void FormMain_Load(object sender, EventArgs e)
         {
             PopulateRecipes();
-            GetIngredients();
+            PopulateIngredients();
         }
 
         private void PopulateRecipes()
@@ -44,10 +44,26 @@ namespace Cookbook
             
         }
 
+        private void PopulateIngredients()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Ingredient", connection))
+            {
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+                listBoxAllIngredients.DisplayMember = "Name";
+                listBoxAllIngredients.ValueMember = "Id";
+                listBoxAllIngredients.DataSource = ingredientTable;
+            }
+
+
+        }
+
         private void listRecipes_SelectedIndexChanged(object sender, EventArgs e)
         {
             GetIngredients();
         }
+        
 
         private void GetIngredients()
         {
@@ -67,6 +83,52 @@ namespace Cookbook
             }
 
 
+        }
+
+        private void buttonAddRecipe_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Recipe VALUES(@RecipeName, 80, 'textHere')";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open(); //needed because no data adapter***
+                command.Parameters.AddWithValue("@RecipeName", textBoxRecipeName.Text);
+                command.ExecuteNonQuery();
+            }
+
+            PopulateRecipes();
+        }
+
+        //to be turned into right click context menu, reusable based on object clicked.
+        private void buttonUpdateName_Click(object sender, EventArgs e)
+        {
+            string query =  "UPDATE Recipe SET Name  = @RecipeName "+
+                            "WHERE Id = @RecipeId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open(); //needed because no data adapter***
+                command.Parameters.AddWithValue("@RecipeName", textBoxRecipeName.Text);
+                command.Parameters.AddWithValue("@RecipeId", listRecipes.SelectedValue);
+                command.ExecuteNonQuery();
+            }
+
+            PopulateRecipes();
+        }
+
+        private void buttonAddToRecipe_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO RecipeIngredient VALUES(@RecipeId, @IngredientId)";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open(); //needed because no data adapter***
+                command.Parameters.AddWithValue("@RecipeId", listRecipes.SelectedValue);
+                command.Parameters.AddWithValue("@IngredientId", listBoxAllIngredients.SelectedValue);
+                command.ExecuteNonQuery();
+            }
+
+            PopulateRecipes();
         }
     }
 }
